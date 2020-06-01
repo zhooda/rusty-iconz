90118ea1937f063136704bfe67b3774a7346b550
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-06-01 13:54:51 -0600

    Added a scale function to generate @2x and @3x properly

--- src/icon.rs
+++ src/icon.rs
@@ -1,11 +1,18 @@
-pub fn resize(src: &str, width: i32, height: i32, name: String, ipad: bool) {
-    println!("{} -> {}", src, &name);
+pub fn resize(src: String, dim: Vec<i32>, name: String, ipad: bool) {
+    println!("{} -> {}", &src, &name);
     let icname = if ipad == true { format!("{}~ipad.png", &name) } else { format!("{}.png", &name) };
     std::process::Command::new("convert")
         .arg(src)
         .arg("-resize")
-        .arg(format!("{}x{}", width, height))
+        .arg(format!("{}x{}", &dim[0], &dim[1]))
         .arg(icname)
         .output()
         .expect("Could not convert");
+}
+
+pub fn scale(src: String, dim: Vec<i32>, name: String, scales: Vec<i32>, ipad: bool) {
+    for scale in scales {
+        let scale_string = if scale > 1 { format!("{}@{}x", &name, &scale)} else { format!("{}", &name) };
+        resize(src.to_owned(), [&dim[0]*scale, &dim[1]*scale].to_vec(), scale_string, ipad);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,16 @@
 mod cli;
 mod icon;
 
-
 fn main() {
     cli::start("iconz", "0.1.1", "make those stupid xcode icons");
 
     let argv: Vec<String> = cli::verify_args(2);
     let src_img: &str = &argv[1];
-    println!("argv: Vec<String> = {:?}", &argv);
+    // println!("argv: Vec<String> = {:?}", &argv);
 
     std::fs::remove_dir_all("./testdir");
     std::fs::create_dir("./testdir");
-    icon::resize(src_img, 200, 200, format!("./testdir/200x200"), false);
+    icon::scale(src_img.to_owned(), [200, 200].to_vec(), "200x200".to_owned(), [2, 3].to_vec(), false);
 
     cli::end();
 }
