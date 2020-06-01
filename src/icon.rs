6fc0e1fa6649e06a53e6d9e948d6895c909d1aab
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-06-01 14:35:00 -0600

    Added func that makes all icons & scales for Xcode

--- src/icon.rs
+++ src/icon.rs
@@ -16,3 +16,21 @@ pub fn scale(src: String, dim: Vec<i32>, name: String, scales: Vec<i32>, ipad: b
         resize(src.to_owned(), [&dim[0]*scale, &dim[1]*scale].to_vec(), scale_string, ipad);
     }
 }
+
+pub fn make_for_xcode(src: &str, dir: &str) {
+    std::fs::remove_dir_all(dir);
+    std::fs::create_dir(dir);
+    scale(src.to_owned(), [20, 20].to_vec(), format!("./{}/AppIcon20x20", dir), [2, 3].to_vec(), false);
+    scale(src.to_owned(), [29, 29].to_vec(), format!("./{}/AppIcon29x29", dir), [2, 3].to_vec(), false);
+    scale(src.to_owned(), [40, 40].to_vec(), format!("./{}/AppIcon40x40", dir), [2, 3].to_vec(), false);
+    scale(src.to_owned(), [60, 60].to_vec(), format!("./{}/AppIcon60x60", dir), [2, 3].to_vec(), false);
+    scale(src.to_owned(), [20, 20].to_vec(), format!("./{}/AppIcon20x20", dir), [1, 2].to_vec(), true);
+    scale(src.to_owned(), [29, 29].to_vec(), format!("./{}/AppIcon29x29", dir), [1, 2].to_vec(), true);
+    scale(src.to_owned(), [40, 40].to_vec(), format!("./{}/AppIcon40x40", dir), [1, 2].to_vec(), true);
+    scale(src.to_owned(), [76, 76].to_vec(), format!("./{}/AppIcon76x76", dir), [1, 2].to_vec(), true);
+    scale(src.to_owned(), [167, 167].to_vec(), format!("./{}/AppIcon83.5x83.5@2x", dir), [1].to_vec(), true);
+    scale(src.to_owned(), [512, 512].to_vec(), format!("./{}/mac", dir), [1].to_vec(), false);
+    scale(src.to_owned(), [1024, 1024].to_vec(), format!("./{}/AppStoreIcon", dir), [1].to_vec(), false);
+    scale(src.to_owned(), [57, 57].to_vec(), format!("./{}/manifest/AppIcon57x57", dir), [1].to_vec(), false);
+    scale(src.to_owned(), [512, 512].to_vec(), format!("./{}/manifest/AppIcon512x512", dir), [1].to_vec(), false);
+}
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,11 @@ fn main() {
     cli::start("iconz", "0.1.1", "make those stupid xcode icons");
 
     let argv: Vec<String> = cli::verify_args(2);
-    let src_img: &str = &argv[1];
-    // println!("argv: Vec<String> = {:?}", &argv);
+    // let src_img: &str = &argv[1];
+    // let dir = &argv[2];
+    let (src_img, dir) = (&argv[1], &argv[2]);
 
-    std::fs::remove_dir_all("./testdir");
-    std::fs::create_dir("./testdir");
-    icon::scale(src_img.to_owned(), [200, 200].to_vec(), "200x200".to_owned(), [2, 3].to_vec(), false);
+    icon::make_for_xcode(src_img, dir);
 
     cli::end();
 }
