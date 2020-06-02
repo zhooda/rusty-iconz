803c9e1ff77aad6c35588dbf561d6ef499e25ba7
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-06-01 22:16:34 -0600

    Added error handling and a bin folder for production releases

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ mod icon;
 mod native;
 
 fn main() {
-    cli::start("iconz", "0.1.2", "make xcode icons\n#blacklivesmatter http://ally.wiki");
 
     let argv: Vec<String> = std::env::args().collect();
 
@@ -19,15 +18,33 @@ fn main() {
     }
     
     if &argv[1] == "-m" || &argv[1] == "--magic" {
-        icon::make_for_xcode(&argv[2], &argv[3]);
+        let is_valid= std::path::Path::new(&argv[2]).exists();
+        if is_valid {
+            cli::start("iconz", "0.1.2", "make xcode icons\n#blacklivesmatter http://ally.wiki");
+            icon::make_for_xcode(&argv[2], &argv[3]);
+        } else {
+            cli::err(format!("[iconz error]: {} is an invalid file path", &argv[2]), -1);
+        }
     // The -i or --image flag is used
     } else if argv.len() == 4 {
-        let buffer = image::open(&argv[2]).unwrap().into_rgb();
-        native::make_for_xcode(buffer, &argv[2], &argv[3]);
+        let is_valid= std::path::Path::new(&argv[2]).exists();
+        if is_valid {
+            cli::start("iconz", "0.1.2", "make xcode icons\n#blacklivesmatter http://ally.wiki");
+            let buffer = image::open(&argv[2]).unwrap().into_rgb();
+            native::make_for_xcode(buffer, &argv[2], &argv[3]);
+        } else {
+            cli::err(format!("[iconz error]: {} is an invalid file path", &argv[2]), -1);
+        }
     // No flags are used -> Defaults to -i instead of -m
     } else {
-        let buffer = image::open(&argv[1]).unwrap().into_rgb();
-        native::make_for_xcode(buffer, &argv[1], &argv[2]);
+        let is_valid= std::path::Path::new(&argv[1]).exists();
+        if is_valid {
+            cli::start("iconz", "0.1.2", "make xcode icons\n#blacklivesmatter http://ally.wiki");
+            let buffer = image::open(&argv[1]).unwrap().into_rgb();
+            native::make_for_xcode(buffer, &argv[1], &argv[2]);
+        } else {
+            cli::err(format!("[iconz error]: {} is an invalid file path", &argv[1]), -1);
+        }
     }
 
     cli::end();
